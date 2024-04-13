cc83c5af93fe93a311de19d1ed374e4c4f7e44f2
Dinis Machado <dinisma@gmail.com>
2024-04-13 01:58:21 +0000

    Workspace set up and under construction message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>öndëgö</h1>
+      <p>Welcome to öndëgö! We are under construction!</p>
+    </div>
+  );
+}
+
+export default App;
