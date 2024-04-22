0bc7b44489286701ba5dd90f9dc0072644af9355
Dinis Machado <dinisma@gmail.com>
2024-04-22 18:34:56 +0000

    Create Profile data contexts

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,41 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+import { useCurrentUser } from "./CurrentUserContext";
+
+export const ProfileDataContext = createContext();
+export const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
+
+export const ProfileDataProvider = ({ children }) => {
+  const [profileData, setProfileData] = useState({
+    pageProfile: { results: [] },
+    latestFriendsLogIn: { results: [] },
+  });
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get("/profiles/?ordering=-last_login");
+        setProfileData((prevState) => ({
+          ...prevState,
+          latestFriendsLogIn: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
+  return (
+    <ProfileDataContext.Provider value={profileData}>
+      <SetProfileDataContext.Provider value={setProfileData}>
+        {children}
+      </SetProfileDataContext.Provider>
+    </ProfileDataContext.Provider>
+  );
+}
--- src/pages/friends/LatestFriendsLogIn.js
+++ src/pages/friends/LatestFriendsLogIn.js
@@ -1,67 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
-import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Profile from "./Profile";
+import { useProfileData } from "../../contexts/ProfileDataContext";
 
-const LatestFriendsLogIn = ({mobile}) => {
-  const [profileData, setProfileData] = useState({
-    pageProfile: { results: [] },
-    latestFriendsLogIn: { results: [] },
-  });
-  const { latestFriendsLogIn } = profileData;
-  const currentUser = useCurrentUser();
+const LatestFriendsLogIn = ({ mobile }) => {
+  const { latestFriendsLogIn } = useProfileData();
 
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get("/profiles/?ordering=-last_login");
-        setProfileData((prevState) => ({
-          ...prevState,
-          latestFriendsLogIn: data,
-        }));
-      } catch (err) {
-        console.log(err);
-      }
-    };
 
-    handleMount();
-  }, [currentUser]);
-
-return (
-  <Container
-    className={`${appStyles.Content} ${
-      mobile && "d-lg-none text-center mb-3"
-    }`}>
-    {latestFriendsLogIn.results.length ? (
-      <>
-        {mobile ? (
-          <div className='d-flex justify-content-around'>
-            {latestFriendsLogIn.results.slice(0, 4).map((profile) => (
+  return (
+    <Container
+      className={`${appStyles.Content} ${
+        mobile && "d-lg-none text-center mb-3"
+      }`}>
+      {latestFriendsLogIn.results.length ? (
+        <>
+          {mobile ? (
+            <div className='d-flex justify-content-around'>
+              {latestFriendsLogIn.results.slice(0, 4).map((profile) => (
+                <Profile
+                  key={profile.id}
+                  profile={profile}
+                  mobile={mobile}
+                />
+              ))}
+            </div>
+          ) : (
+            latestFriendsLogIn.results.map((profile) => (
               <Profile
                 key={profile.id}
                 profile={profile}
                 mobile={mobile}
               />
-            ))}
-          </div>
-        ) : (
-          latestFriendsLogIn.results.map((profile) => (
-            <Profile
-              key={profile.id}
-              profile={profile}
-              mobile={mobile}
-            />
-          ))
-        )}
-      </>
-    ) : (
-      <Asset spinner />
-    )}
-  </Container>
-);
+            ))
+          )}
+        </>
+      ) : (
+        <Asset spinner />
+      )}
+    </Container>
+  );
 };
 
 export default LatestFriendsLogIn;
