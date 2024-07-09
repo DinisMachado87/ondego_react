35942f1ee0c1c91362b3eb5f8605ea334c256ee6
Dinis Machado <137174511+DinisMachado87@users.noreply.github.com>
2024-07-09 22:20:38 +0200

    Add spinner to event page loading

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -15,10 +15,12 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Comment from "../comments/Comment";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import Asset from "../../components/Asset";
 
 function EventPage() {
   const { id } = useParams();
   const [event, setEvent] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
 
   const currentUser = useCurrentUser();
   const profile_image = currentUser?.profile_image;
@@ -33,6 +35,7 @@ function EventPage() {
         ]);
         setEvent({ results: [event] });
         setComments(comments);
+        setHasLoaded(true);
       } catch (err) {
         console.log(err);
       }
@@ -44,42 +47,46 @@ function EventPage() {
   return (
     <Row>
       <Col className='py-2 p-0 p-lg-2'>
-        <Event
-          {...event.results[0]}
-          setEvents={setEvent}
-          EventPage
-        />
         <Container>
-          {currentUser && (
-            <EventCommentCreateForm
-              event={event.results[0]}
-              profile_id={currentUser.profile_id}
-              profileImage={profile_image}
-              post={id}
-              setEvent={setEvent}
-              setComments={setComments}
+          {hasLoaded && event.results.length ? (
+            <Event
+              {...event.results[0]}
+              eventPage
+              setEvents={setEvent}
             />
+          ) : (
+            <Asset spinner />
           )}
-          <div className={`${styles.CommentsScrollingContainer} p-2`}>
-            <InfiniteScroll
-              dataLength={comments.results.length}
-              next={() => fetchMoreData(comments, setComments)}
-              hasMore={!!comments.next}
-              loader={<span>Loading more comments...</span>}>
-              {comments.results.length ? (
-                comments.results.map((comment) => (
-                  <Comment
-                    key={comment.id}
-                    {...comment}
-                    setEvent={setEvent}
-                    setComments={setComments}
-                  />
-                ))
-              ) : (
-                <span>No comments yet. Be the first to comment!</span>
-              )}
-            </InfiniteScroll>
-          </div>
+        {currentUser && (
+          <EventCommentCreateForm
+            event={event.results[0]}
+            profile_id={currentUser.profile_id}
+            profileImage={profile_image}
+            post={id}
+            setEvent={setEvent}
+            setComments={setComments}
+          />
+        )}
+        <div className={`${styles.CommentsScrollingContainer} p-2`}>
+          <InfiniteScroll
+            dataLength={comments.results.length}
+            next={() => fetchMoreData(comments, setComments)}
+            hasMore={!!comments.next}
+            loader={<span>Loading more comments...</span>}>
+            {comments.results.length ? (
+              comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setEvent={setEvent}
+                  setComments={setComments}
+                />
+              ))
+            ) : (
+              <span>No comments yet. Be the first to comment!</span>
+            )}
+          </InfiniteScroll>
+        </div>
         </Container>
       </Col>
     </Row>
